f88fd34b1fafa92795022e8e667d01f8306169c9
Jay <Jay@192.168.0.89>
2018-04-14 17:43:35 -0300

    Agrego clase repositorio y ultimas modificaciones hechas en diagramas

---
+++ src/main/java/modelo/Repositorio.java
@@ -0,0 +1,31 @@
+package modelo;
+
+import java.util.List;
+
+/**
+ * @author Jay
+ * @version 1.0
+ * @created 13-Apr-2018 8:57:52 PM
+ */
+public class Repositorio {
+
+	private List<Cliente> clientes;
+	private List<Dispositivo> dispositivos;
+
+	public Repositorio(){
+
+	}
+
+
+	public List<Cliente> getClientes(){
+		return null;
+	}
+
+	public Dispositivo getDispositivoById(){
+		return null;
+	}
+
+	public List<Dispositivo> getDispositivos(){
+		return null;
+	}
+}//end Repositorio
